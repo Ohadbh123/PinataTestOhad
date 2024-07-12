a41e6de938183899118316d94eeaf355fbc93984
ohad <ohadbh123@gmail.com>
2024-07-12 12:16:11 +0300

    ui adjustments and level selector

---
+++ Assets/Scripts/LevelButtonView.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LevelButtonView : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    [SerializeField] private TMP_Text _levelText;
+
+    public void SetLevelIndex(int levelIndex)
+    {
+        _levelText.text = levelIndex.ToString();
+    }
+
+    public void AddButtonListener(UnityAction<int> action, int levelIndex)
+    {
+        _button.onClick.AddListener(delegate { action(levelIndex); });
+    }
+    
+    public void RemoveButtonListener(UnityAction<int> action, int levelIndex)
+    {
+        _button.onClick.RemoveListener(delegate { action(levelIndex); });
+    }
+}
