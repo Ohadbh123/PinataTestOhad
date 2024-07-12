30f99373c2ee97a3130d8e940305400e8c46b26d
ohad <ohadbh123@gmail.com>
2024-07-12 20:47:19 +0300

    vfx and pinata

--- Assets/Scripts/Gameplay/PinataController.cs
+++ Assets/Scripts/Gameplay/PinataController.cs
@@ -6,11 +6,16 @@ namespace Gameplay
 {
     public class PinataController : MonoBehaviour , IInteractable
     {
+        private static readonly int _animatorHitTrigger = Animator.StringToHash("Hit");
+
+        [SerializeField] private Animator _animator;
+        
         [HideInInspector]
         public UnityEvent OnPinataAchieved;
         
         public void Interact(ProjectileController projectileController)
         {
+            _animator.SetTrigger(_animatorHitTrigger);
             projectileController.DestroyProjectile();
             OnPinataAchieved.Invoke();
         }
